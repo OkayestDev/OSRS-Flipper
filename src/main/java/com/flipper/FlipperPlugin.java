a4762c55f1bec66d929826f492c23c39bba7ec62
Kyle Richardson <richardson.a.kyle.95@gmail.com>
2020-06-08 04:12:04 -0400

    Changing to spaces

--- src/main/java/com/flipper/FlipperPlugin.java
+++ src/main/java/com/flipper/FlipperPlugin.java
@@ -24,77 +24,77 @@ import net.runelite.client.ui.ClientToolbar;
 
 @PluginDescriptor(name = "Flipper")
 public class FlipperPlugin extends Plugin {
-	// Injects
-	@Inject
-	private Client client;
-	@Inject
-	private FlipperConfig config;
-	@Inject
-	private ScheduledExecutorService executor;
-	@Inject
-	private ClientThread clientThread;
-	@Inject
-	private ClientToolbar clientToolbar;
-	@Inject
-	private ItemManager itemManager;
-	// Controllers
-	private BuysController buysController;
-	private SellsController sellsController;
-	private TabManagerController tabManagerController;
+    // Injects
+    @Inject
+    private Client client;
+    @Inject
+    private FlipperConfig config;
+    @Inject
+    private ScheduledExecutorService executor;
+    @Inject
+    private ClientThread clientThread;
+    @Inject
+    private ClientToolbar clientToolbar;
+    @Inject
+    private ItemManager itemManager;
+    // Controllers
+    private BuysController buysController;
+    private SellsController sellsController;
+    private TabManagerController tabManagerController;
 
-	/** @todo figure how to run panel updates on dispatch thread */
-	@Override
-	protected void startUp() throws Exception {
-		TradePersister.setUp();
-		buysController = new BuysController(itemManager);
-		sellsController = new SellsController(itemManager);
-		tabManagerController = new TabManagerController(
-			clientToolbar,
-			buysController.getPanel(),
-			sellsController.getPanel()
-		);
-	}
+    /** @todo figure how to run panel updates on dispatch thread */
+    @Override
+    protected void startUp() throws Exception {
+        TradePersister.setUp();
+        buysController = new BuysController(itemManager);
+        sellsController = new SellsController(itemManager);
+        tabManagerController = new TabManagerController(
+            clientToolbar,
+            buysController.getPanel(),
+            sellsController.getPanel()
+        );
+    }
 
-	private void saveAll() {
-		buysController.saveBuys();
-		sellsController.saveSells();
-	}
+    private void saveAll() {
+        buysController.saveBuys();
+        sellsController.saveSells();
+    }
 
-	@Override
-	protected void shutDown() throws Exception {
-		Log.info("Flipper stopped!");
-		this.saveAll();
-	}
+    @Override
+    protected void shutDown() throws Exception {
+        Log.info("Flipper stopped!");
+        this.saveAll();
+    }
 
-	/**
-	 * Ensure we save transactions when the client is being shutdown. Prevents main
-	 * thread from continue until transactions have been saved
-	 *
-	 * @param clientShutdownEvent even that we receive when the client is shutting
-	 *                            down
-	 * @todo implement
-	 */
-	@Subscribe(priority = 101)
-	public void onClientShutdown(ClientShutdown clientShutdownEvent) {
-		this.saveAll();
-	}
+    /**
+     * Ensure we save transactions when the client is being shutdown. Prevents main
+     * thread from continue until transactions have been saved
+     *
+     * @param clientShutdownEvent even that we receive when the client is shutting
+     *                            down
+     * @todo implement
+     */
+    @Subscribe(priority = 101)
+    public void onClientShutdown(ClientShutdown clientShutdownEvent) {
+        this.saveAll();
+    }
 
-	@Subscribe
-	public void onGrandExchangeOfferChanged(GrandExchangeOfferChanged newOfferEvent) {
-		GrandExchangeOffer offer = newOfferEvent.getOffer();
-		// Ignore empty state event offers or offers that haven't bought/sold any
-		if (offer.getState() != GrandExchangeOfferState.EMPTY && offer.getQuantitySold() != 0) {
-			boolean isBuy = GrandExchange.checkIsBuy(offer.getState());
-			if (isBuy) {
-				buysController.createBuy(offer);
-			} else {
-				sellsController.createSell(offer);
-			}
-		}
-	}
+    @Subscribe
+    public void onGrandExchangeOfferChanged(GrandExchangeOfferChanged newOfferEvent) {
+        GrandExchangeOffer offer = newOfferEvent.getOffer();
+        // Ignore empty state event offers or offers that haven't bought/sold any
+        if (offer.getState() != GrandExchangeOfferState.EMPTY && offer.getQuantitySold() != 0) {
+            boolean isBuy = GrandExchange.checkIsBuy(offer.getState());
+            if (isBuy) {
+                buysController.createBuy(offer);
+            } else {
+                sellsController.createSell(offer);
+            }
+        }
+    }
 
-	@Provides
-	FlipperConfig provideConfig(ConfigManager configManager) {
-		return configManager.getConfig(FlipperConfig.class);
-	}
+    @Provides
+    FlipperConfig provideConfig(ConfigManager configManager) {
+        return configManager.getConfig(FlipperConfig.class);
+    }
 }
