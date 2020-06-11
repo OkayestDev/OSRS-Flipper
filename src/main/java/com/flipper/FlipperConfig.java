61e4f9b8ba1c88de18e6024cda280ceda7359942
Kyle Richardson <richardson.a.kyle.95@gmail.com>
2020-06-11 12:49:19 -0400

    Removing example config. Leaving file as I am adding configurations later

--- src/main/java/com/flipper/FlipperConfig.java
+++ src/main/java/com/flipper/FlipperConfig.java
@@ -2,18 +2,9 @@ package com.flipper;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("flipper")
 public interface FlipperConfig extends Config
 {
-    @ConfigItem(
-        keyName = "greeting",
-        name = "Welcome Greeting",
-        description = "The message to show to the user when they login"
-    )
-    default String greeting()
-    {
-        return "Hello";
-    }
+
 }
